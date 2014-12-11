3ff27a62beb01d1aae8c9ca8a2b1b5e87092a9b1
dwendle <wendykung225@gmail.com>
2014-12-10 18:56:29 -0800

    added search queries

--- js/test.js
+++ js/test.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-    // $('img.circle').hover(
-    //     function() {
-    //         $(this).addClass('main-hover');
-    //         $(this).next().addClass('showName');
-    //     }, function() {
-    //         $(this).removeClass('main-hover');
-    //         $(this).next().removeClass('showName');
-    // });
     $('#strength').click(function() {
         search( 'weights', $( "#linkResults " ), $( ".template.result" ) );
         $('.circle').addClass('move-button');
@@ -18,32 +10,25 @@ $(document).ready(function(){
         $('.sectionResults').removeClass('showSection');
         $('div#strengthResults').addClass('showSection');
         $('div.result').removeClass('filter');
-        // $('img.img-circle').hover(
-        //     function() {
-        //         $(this).addClass('change-hover');
-        //         $(this).next().addClass('showName');
-        //     }, function() {
-        //         $(this).removeClass('change-hover');
-        //         $(this).next().removeClass('showName');
-        // });
         $('a#tab2').text('Arms');
         $('a#tab3').text('Legs');
         $('a#tab4').text('Core');
         $('a#tab1').click(function() {
+            search( 'weights', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').removeClass('filter');
         });
         $('a#tab2').click(function() {
-            // search( 'arms', $( "#linkResults " ), $( ".template.result" ) );
+            search( 'arms', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.arms').removeClass('filter');
         });
         $('a#tab3').click(function() {
-            // search( 'legs', $( "#linkResults " ), $( ".template.result" ) );
+            search( 'legs', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.legs').removeClass('filter');
         });
         $('a#tab4').click(function() {
-            // search( 'core', $( "#linkResults " ), $( ".template.result" ) );
+            search( 'core', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.core').removeClass('filter');
         });
@@ -63,29 +48,25 @@ $(document).ready(function(){
         $('.sectionResults').removeClass('showSection');
         $('div#stretchResults').addClass('showSection');
         $('div.result').removeClass('filter');
-        // $('img.img-circle').hover(
-        //     function() {
-        //         $(this).addClass('change-hover');
-        //         $(this).next().addClass('showName');
-        //     }, function() {
-        //         $(this).removeClass('change-hover');
-        //         $(this).next().removeClass('showName');
-        // });
         $('a#tab2').text('Upper Body');
         $('a#tab3').text('Core');
         $('a#tab4').text('Lower Body');
         $('a#tab1').click(function() {
+            search( 'stretch', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').removeClass('filter');
         });
         $('a#tab2').click(function() {
+            search( 'yoga', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.upper').removeClass('filter');
         });
         $('a#tab3').click(function() {
+            search( 'pilates', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.mid').removeClass('filter');
         });
         $('a#tab4').click(function() {
+            search( 'vinyasa', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.lower').removeClass('filter');
         });
@@ -105,29 +86,25 @@ $(document).ready(function(){
         $('.sectionResults').removeClass('showSection');
         $('div#cardioResults').addClass('showSection');
         $('div.result').removeClass('filter');
-        // $('img.img-circle').hover(
-        //     function() {
-        //         $(this).addClass('change-hover');
-        //         $(this).next().addClass('showName');
-        //     }, function() {
-        //         $(this).removeClass('change-hover');
-        //         $(this).next().removeClass('showName');
-        // });
         $('a#tab2').text('At Home');
         $('a#tab3').text('In The Gym');
         $('a#tab4').text('Outdoors');
         $('a#tab1').click(function() {
+            search( 'cardio', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').removeClass('filter');
         });
         $('a#tab2').click(function() {
+            search( 'dance', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.home').removeClass('filter');
         });
         $('a#tab3').click(function() {
+            search( 'tredmill', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.gym').removeClass('filter');
         });
         $('a#tab4').click(function() {
+            search( 'cycling', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.out').removeClass('filter');
         });
@@ -147,29 +124,25 @@ $(document).ready(function(){
         $('.sectionResults').removeClass('showSection');
         $('div#quickResults').addClass('showSection');
         $('div.result').removeClass('filter');
-        // $('img.img-circle').hover(
-        //     function() {
-        //         $(this).addClass('change-hover');
-        //         $(this).next().addClass('showName');
-        //     }, function() {
-        //         $(this).removeClass('change-hover');
-        //         $(this).next().removeClass('showName');
-        // });
         $('a#tab2').text('5-10 Min');
         $('a#tab3').text('10-15 Min');
         $('a#tab4').text('15-30 Min');
         $('a#tab1').click(function() {
+            search( 'quick', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').removeClass('filter');
         });
         $('a#tab2').click(function() {
+            search( 'intense', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.short').removeClass('filter');
         });
         $('a#tab3').click(function() {
+            search( 'sprint', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.middle').removeClass('filter');
         });
         $('a#tab4').click(function() {
+            search( 'endurance', $( "#linkResults " ), $( ".template.result" ) );
             $('div.result').addClass('filter');
             $('.long').removeClass('filter');
         });
